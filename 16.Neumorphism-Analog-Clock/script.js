61d58434e25b7d924bef3e11e2abb24bb98f0e85
HA NGUYEN <hanguyentieuyen@gmail.com>
2021-08-01 20:25:59 +0700

    update code

---
+++ 16.Neumorphism-Analog-Clock/script.js
@@ -0,0 +1,22 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(()=>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;   //Quay ngược chiều kim đồng hồ : rotateZ(góc dương)
+    sc.style.transform =  `rotateZ(${ss}deg)`;
+
+})
+
+
+function toggleClass(){
+    const body = document.querySelector('body');
+    body.classList.toggle('light');
+}
